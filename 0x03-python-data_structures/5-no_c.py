f7173437e1a65eb6c9cf6cd38fa8dc78b282a574
adamgado <adamgado33@gmail.com>
2023-12-04 10:18:14 -0800

    fix for 0, 1 and 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new = my_strange.translate({ord(a): None for a in 'cC'})
-    return new
+    s_new = my_strange.translate({ord(a): None for a in 'cC'})
+    return s_new
