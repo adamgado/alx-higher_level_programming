6936c1ce8105c8abab4075eaff689d5f477bde16
adamgado <adamgado33@gmail.com>
2023-12-04 10:27:29 -0800

    5 fixed, for sure this time

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    s_new = my_stringe.translate({ord(a): None for a in 'cC'})
+    s_new = my_string.translate({ord(a): None for a in 'cC'})
     return s_new
