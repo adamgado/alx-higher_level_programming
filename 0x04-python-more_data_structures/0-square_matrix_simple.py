e5355eb6cba6a25d197ff7dcf38e21f3eb42ab88
adamgado <adamgado33@gmail.com>
2023-12-06 10:03:18 -0800

    0 fix 1

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    return ([list(map(lambda x: x * x, row)) for row in matrix])
+    new_matrix = []
+    for a in matrix[:]:
+        new_matrix.append(list(map(lambda x: x * x, row)))
+    return new_matrix
