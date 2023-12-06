2fdce288b510cf321681b94ee30e30a3eb0d72f9
adamgado <adamgado33@gmail.com>
2023-12-06 15:05:17 -0800

    10 fix 3

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,7 +2,7 @@
 def best_score(a_dictionary):
     if len(a_dictionary) == 0:
         return None
-     score = max(a_dictionary.values())
+    score = max(a_dictionary)
     for a in a_dictionary:
         if a_dictionary[a] == score:
             return a
