222255de1e25007d9ce73b0da5452bdde41dd7b3
adamgado <adamgado33@gmail.com>
2023-12-18 08:06:11 -0800

    0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    try:
+        while a is not x:
+            print(my_list[a], end='')
+            a = a + 1
+    except IndexError:
+        None
+    return a
