fdbab608b0802134e7331d532eba218f22de9f1f
adamgado <adamgado33@gmail.com>
2023-12-19 07:21:37 -0800

    1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""defines a 'Square' class"""
+
+
+class Square:
+    """defines a square"""
+    def __init__(self, size):
+        """Initializes a new Square.
+
+        Args:
+            size : size of the new square.
+        """
+        self.__size = size
