44851ea2c1a57108c9d07100fdb5d79ba60a9231
adamgado <adamgado33@gmail.com>
2024-01-03 14:51:15 -0800

    3 exec

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Module for print_square function"""
+
+
+def print_square(size):
+    """print a square with #s
+
+    Args:
+        size: int size of square's side
+
+    Raises:
+        TypeError: size is not int
+        ValueError: size is negative
+    """
+    if type(size) not in (int):
+        raise TypeError("size must be an integer")
+
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    print((("#" * size + "\n") * size), end="")
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/4-print_square.txt")
