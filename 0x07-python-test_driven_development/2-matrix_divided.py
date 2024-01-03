411f140df8a6318d1c57c22812b465871dfc05ad
adamgado <adamgado33@gmail.com>
2024-01-03 15:01:18 -0800

    1 fix

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -15,19 +15,19 @@ def matrix_divided(matrix, div):
         TypeError: sublists are not all same size
         ZeroDivisionError: div is zero
     """
-    if type(div) not in (int, float):
+    if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
-    iif type(matrix) not in (list) or len(matrix) == 0:
+    if not isinstance(matrix, list) or len(matrix) == 0:
         raise TypeError("matrix must be a matrix (list of lists)" +
                         "of integers/floats")
     for x in matrix:
-            if type(x) not in (list) or len(x) == 0:
+        if not isinstance(x, list) or len(x) == 0:
             raise TypeError("matrix must be a matrix (list of lists) " +
                             "of integers/floats")
         if len(x) != len(matrix[0]):
             raise TypeError("Each row of the matrix must have the same size")
         for y in x:
-            if type(y) not in (int, float)):
+            if not isinstance(y, (int, float)):
                 raise TypeError("matrix must be a matrix (list of lists) " +
                                 "of integers/floats")
     return [[round(y / div, 2) for y in x] for x in matrix]
