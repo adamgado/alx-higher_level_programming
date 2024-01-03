ddc80b4eea3c9da4c4305947e526ce93497513a6
adamgado <adamgado33@gmail.com>
2024-01-03 14:52:25 -0800

    4 exec

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Module for text_indentation function"""
+
+
+def text_indentation(text):
+    """add 2 lines after '.', '?' and ':'
+
+    Args:
+        text: string
+
+    Raises:
+        TypeError: text is not string
+    """
+    if type(text) not in (str):
+        raise TypeError("text must be a string")
+
+    for a in ".?:":
+        # print(a, text.split(a))
+        text = (a + "\n\n").join(
+            [line.strip(" ") for line in text.split(a)])
+
+    print(text, end="")
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/5-text_indentation.txt")
