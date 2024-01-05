fb1c52bbf55b98b522929ea6f62980075424b7d8
adamgado <adamgado33@gmail.com>
2024-01-05 02:56:38 -0800

    100 fix, first adv

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def magic_string():
-    count = getattr(magic_string, 'count', 0) + 1
-    return ", ".join("BestSchool" for a in range(count))
+    magic_string.count = getattr(magic_string, 'count', 0) + 1
+    return ", ".join("BestSchool" for a in range(magic_string.count))
