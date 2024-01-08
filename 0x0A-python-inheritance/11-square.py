ce76c362f08dd1bc8e60a840d4dd565bca78ba9d
adamgado <adamgado33@gmail.com>
2024-01-08 01:06:21 -0800

    11

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""square class"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """square class inherits from rectangle"""
+    def __init__(self, size):
+        """initialize size"""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """square area"""
+        return self.__size ** 2
+
+ def __str__(self):
+        """string representation of square"""
+        return "[Square] " + str(self.__size) + "/" + str(self.__size)
