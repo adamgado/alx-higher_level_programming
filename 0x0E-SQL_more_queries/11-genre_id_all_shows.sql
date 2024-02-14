5b11dc2b526296f64154b26d8c0ec54b3bc50a91
adamgado <adamgado33@gmail.com>
2024-02-14 08:41:24 -0800

    10 -> 13

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,2 @@
+-- list all shows contained that have optional genre
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows LEFT OUTER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
