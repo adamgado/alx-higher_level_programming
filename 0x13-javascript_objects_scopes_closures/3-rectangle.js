fe521eaa9b4a3cd926321635a210195e9b8648a1
adamgado <adamgado33@gmail.com>
2024-03-12 00:54:52 -0700

    0 -> 5

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (typeof w === 'number' && typeof h === 'number' && w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+  print () {
+    for (let a = 0; a < this.height; ++a) {
+      let b = 0;
+      for (; b < this.width; ++b) {
+        process.stdout.write('X');
+      }
+      if (b === this.width) {
+        console.log('');
+      }
+    }
+  }
+};
---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (typeof w === 'number' && typeof h === 'number' && w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+  print () {
+    for (let a = 0; a < this.height; ++a) {
+      let b = 0;
+      for (; b < this.width; ++b) {
+        process.stdout.write('X');
+      }
+      if (b === this.width) {
+        console.log('');
+      }
+    }
+  }
+  rotate () {
+    [this.width, this.height] = [this.height, this.width];
+  }
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+};
