426851e7ebd9e6a37e54f6dc3401e324c0e0937d
adamgado <adamgado33@gmail.com>
2024-03-12 03:02:01 -0700

    101 fix

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 const dict = require('./101-data.js').dict;
-let new_d = {};
-for (let a in dict) {
-  if (new_d[dict[a]] === undefined) {
-    new_d[dict[a]] = [a];
+const newD = {};
+for (const a in dict) {
+  if (newD[dict[a]] === undefined) {
+    newD[dict[a]] = [a];
   } else {
-    new_d[dict[a]].push(a);
+    newD[dict[a]].push(a);
   }
 }
-console.log(new_d);
+console.log(newD);
