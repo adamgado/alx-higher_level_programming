4127b3a1b314b148c6b3244ad846ec94d378cc2f
adamgado <adamgado33@gmail.com>
2024-03-14 05:37:56 -0700

    5

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""lists cities from database"""
+import MySQLdb
+from sys import argv
+
+
+if __name__ == '__main__':
+    """get the states from the database"""
+    db_list = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                              passwd=argv[2], db=argv[3])
+    x = db_list.cursor()
+    x.execute("""SELECT cities.name FROM
+              cities INNER JOIN states ON states.id=cities.state_id
+              WHERE states.name=%s""", (sys.argv[4],))
+    cities_db = x.fetchall()
+    temp = list(a[0] for a in cities_db)
+    print(*temp, sep=", ")
