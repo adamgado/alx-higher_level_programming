850569a4ff0271c659a322ac8291e13d932a6220
adamgado <adamgado33@gmail.com>
2024-03-14 04:39:18 -0700

    0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,7 +7,7 @@ from sys import argv
 if __name__ == '__main__':
     """get the states from the database"""
     db_list = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                         passwd=argv[2], db=argv[3])
+                              passwd=argv[2], db=argv[3])
     x = db_list.cursor()
     x.execute("SELECT * FROM states")
     states_db = x.fetchall()
