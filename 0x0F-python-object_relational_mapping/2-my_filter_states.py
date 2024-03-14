1640dfd8426e2c7c65514ac76c9227d3c925faf9
adamgado <adamgado33@gmail.com>
2024-03-14 04:43:22 -0700

    2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""lists states matching argument from database"""
+"""lists states from database matching argument"""
 import MySQLdb
 from sys import argv
 
@@ -7,11 +7,10 @@ from sys import argv
 if __name__ == '__main__':
     """get the states from the database"""
     db_list = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                         passwd=argv[2], db=argv[3])
+                              passwd=argv[2], db=argv[3])
     x = db_list.cursor()
-    x.execute("SELECT * FROM states \
-               WHERE name LIKE BINARY '{}' \
-               ORDER BY states.id ASC".format(argv[4]))
+    x.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
+              .format(sys.argv[4]))
     states_db = x.fetchall()
     for a in states_db:
         print(a)
