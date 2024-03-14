189c71820046ca5bbb2f764a74eb974fd0dc1533
adamgado <adamgado33@gmail.com>
2024-03-14 08:14:39 -0700

    8

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Start link class to table in database"""
+"""prints the first State object"""
 import sys
 from model_state import Base, State
 from sqlalchemy import create_engine
