07887a6c8f708f3225c30549bba928c6ee5c34c0
adamgado <adamgado33@gmail.com>
2024-03-14 08:21:20 -0700

    3

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -5,7 +5,6 @@ from sys import argv
 
 
 if __name__ == '__main__':
-    """get the states from the database"""
     db_list = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                               passwd=argv[2], db=argv[3])
     x = db_list.cursor()
