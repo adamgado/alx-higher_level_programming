8c45ba61c90c3125a5b23121ea43916f0f3d81a2
adamgado <adamgado33@gmail.com>
2024-03-14 02:31:24 -0700

    0

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""lists all states from database"""
+import MySQLdb
+from sys import argv
+
+
+if __name__ == '__main__':
+    """get the states from the database"""
+    db_list = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
+    x = db_list.cursor()
+    x.execute("SELECT * FROM states")
+    states_db = x.fetchall()
+    for a in states_db:
+        print(a)
