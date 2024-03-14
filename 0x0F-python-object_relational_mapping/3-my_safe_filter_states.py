9a984bc1d6238b1245be3eb1f5730de976bd3f12
adamgado <adamgado33@gmail.com>
2024-03-14 02:52:29 -0700

    3

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""lists states starting from database without sql injection"""
+import MySQLdb
+from sys import argv
+
+
+if __name__ == '__main__':
+    """get the states from the database"""
+    db_list = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
+    x = db_list.cursor()
+    x.execute("SELECT * FROM states WHERE name LIKE %s", (match, ))
+    states_db = x.fetchall()
+    for a in states_db:
+        print(a)
