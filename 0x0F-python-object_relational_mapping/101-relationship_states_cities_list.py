cafbc7cdf3d67064073e8f63c28cf7631808bf3d
adamgado <adamgado33@gmail.com>
2024-03-16 03:42:21 -0700

    101 fix

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """prints all City objects from the database"""
 import sys
-from model_state import Base, State
+from relationship_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_city import City
+from relationship_city import City
 
 
 if __name__ == "__main__":
