59729ce690c579ee5aedf7b13d79e06ebdc9ac59
adamgado <adamgado33@gmail.com>
2024-03-16 03:02:31 -0700

    100 more fixes

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -9,7 +9,8 @@ from model_city import City
 
 if __name__ == "__main__":
     db_engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                              .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+                              .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                              pool_pre_ping=True)
     Session = sessionmaker(bind=db_engine)
     session = Session()
     new_s = State(name='California')
