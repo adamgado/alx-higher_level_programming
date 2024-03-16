e8b054017a1224b797746a6a252f2718a3c53ff8
adamgado <adamgado33@gmail.com>
2024-03-16 02:59:18 -0700

    100

---
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""prints all City objects from the database"""
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_city import City
+
+
+if __name__ == "__main__":
+    db_engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                              .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Session = sessionmaker(bind=db_engine)
+    session = Session()
+    new_s = State(name='California')
+    new_c = City(name='San Francisco')
+    new_s.cities.append(new_c)
+    session.add(new_s)
+    session.add(new_c)
+    session.commit()
---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Contains State class and Base"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
+
+
+class State(Base):
+    """class state"""
+    __tablename__ = 'states'
+    id = Column(Integer, unique=True, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="all, delete")
