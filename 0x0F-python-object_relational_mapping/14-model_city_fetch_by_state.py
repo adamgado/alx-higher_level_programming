a45a32ce92fb0f7dec36b33368ce976547d59bec
adamgado <adamgado33@gmail.com>
2024-03-16 02:51:20 -0700

    14 last fixes

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -2,8 +2,9 @@
 """prints all City objects from the database"""
 import sys
 from model_state import Base, State
-from sqlalchemy import (create_engine)
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from model_city import City
 
 
 if __name__ == "__main__":
@@ -11,10 +12,6 @@ if __name__ == "__main__":
                               .format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Session = sessionmaker(bind=db_engine)
     session = Session()
-    query = session.query(City, State).join(State)
-
-    for _c, _s in query.all():
-        print("{}: ({:d}) {}".format(_s.name, _c.id, _c.name))
-
-    session.commit()
-    session.close()
+    for a in (session.query(State.name, City.id, City.name)
+              .filter(State.id == City.state_id)):
+        print("{}: ({}) {}".format(a[0], str(a[1]), a[2]))
