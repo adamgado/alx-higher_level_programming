6af7f79bff8a4e644bb04f0c8dab3fa1dc353e1d
adamgado <adamgado33@gmail.com>
2024-03-28 13:50:04 -0700

    9 fix

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -2,11 +2,10 @@
 """takes your GitHub credentials and uses the GitHub API to display your id"""
 import requests
 from sys import argv
-from requests.auth import HTTPBasicAuth
 
 
 if __name__ == "__main__":
-    a = HTTPBasicAuth(argv[1], argv[2])
-    lnk = requests.get("https://api.github.com/user", auth=a)
+    lnk = requests.get("https://api.github.com/user",
+                       auth=(argv[1], argv[2])).json()
 
-    print(lnk.json().get('id'))
+    print(lnk.get('id'))
