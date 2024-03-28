781e5be5c5879a94bda343864ea77e9591a5bdee
adamgado <adamgado33@gmail.com>
2024-03-28 13:17:36 -0700

    4 fix 2

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -4,7 +4,7 @@ import requests
 
 
 if __name__ == "__main__":
-    lnk = requests.get("https://alx-intranet.hbtn.io/status")
+    lnk = requests.get('https://alx-intranet.hbtn.io/status')
 
     print('Body response:')
     print('\t- type: {}'.format(type(lnk.text)))
