06c89bc02033df93b9c34fa3424531618c4e604c
adamgado <adamgado33@gmail.com>
2024-03-28 10:21:57 -0700

    0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""fetches https://alx-intranet.hbtn.io/status"""
+from urllib.request import Request, urlopen
+
+
+if __name__ == "__main__":
+    lnk = Request("https://alx-intranet.hbtn.io/status")
+    with urlopen(lnk) as r:
+        content = r.read()
+        print("content response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode("utf-8")))
