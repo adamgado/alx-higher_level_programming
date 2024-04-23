af02d6aa3fb6a457be1273db55e3b88fb6750303
adamgado <adamgado33@gmail.com>
2024-04-23 09:54:52 -0700

    3

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-const request = require('request');
-const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2] + '/';
-request(url, function (error, response, body) {
+const req = require('request');
+const link = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2] + '/';
+req(link, function (error, response, body) {
   console.log(error || JSON.parse(body).title);
 });
