7b09509cd30e763a45a63cffdd122fcc62da326b
adamgado <adamgado33@gmail.com>
2024-04-23 09:39:09 -0700

    0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const file_s = require('fs');
-file_s.readFile(process.argv[2], 'utf8', function (error, content) {
+const file = require('fs');
+file.readFile(process.argv[2], 'utf8', function (error, content) {
   console.log(error || content);
 });
