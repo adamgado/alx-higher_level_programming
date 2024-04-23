2e0fb8af1b92e94d8418d8725336a75f929bb764
adamgado <adamgado33@gmail.com>
2024-04-23 10:13:47 -0700

    6

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -3,11 +3,11 @@ const req = require('request');
 req(process.argv[2], function (error, response, body) {
   if (!error) {
     const todolist = JSON.parse(body);
-    const tododone = {};
+    let tododone = {};
     todolist.forEach((todo) => {
-      if (todo.tododone && tododone[todo.userId] === undefined) {
+      if (todo.completed && tododone[todo.userId] === undefined) {
         tododone[todo.userId] = 1;
-      } else if (todo.tododone) {
+      } else if (todo.completed) {
         tododone[todo.userId] += 1;
       }
     });
